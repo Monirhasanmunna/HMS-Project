9092cb404044af3cdd98627f46b0a57456db2c31
raisdhaka@gmail.com <raisdhaka@gmail.com>
2022-10-29 10:56:54 +0600

    invoice, invoice detail

---
+++ database/migrations/2022_10_29_044357_create_invoices_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('doctor_id')->constrained('doctors')->onDelete('cascade');
+            $table->foreignId('patient_id')->constrained('patients')->onDelete('cascade');
+            $table->enum('invoice_type',['bed', 'prescription'])->default('prescription');
+            $table->date('invoice_date');
+            $table->date('admission_date')->nullable();
+            $table->string('remark');
+            $table->double('amount', 8,2);
+            $table->double('discount', 8,2);
+            $table->double('net_amount', 8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
